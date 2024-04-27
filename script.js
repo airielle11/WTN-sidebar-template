7a87f6f890dfba3e3ad2b568f05e4bc26a457b81
airielle <122017843+elruin@users.noreply.github.com>
2024-04-27 14:59:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
+// for hover effect
 const sidebarTabs = document.querySelectorAll('.sidebar .nav-item a');
 
 sidebarTabs.forEach(tab => {
-    tab.addEventListener('click', function() {
-        sidebarTabs.forEach(tab => tab.classList.remove('active'));
-        this.classList.add('active');
-    });
+  tab.addEventListener('click', function () {
+    sidebarTabs.forEach(tab => tab.classList.remove('active'));
+    this.classList.add('active');
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const menuBtn = document.querySelectorAll(".menu-btn");
-    const sidebar = document.querySelector(".sidebar");
-    const bodyOverlay = document.querySelector(".body-overlay");
-    const mainContent = document.getElementById("main-content");
-  
-    const showSidebar = () => {
-      sidebar.classList.toggle('active');
-      mainContent.classList.toggle('active');
-      sidebar.classList.toggle('show-nav');
-      bodyOverlay.classList.toggle('show-nav');
-    };
-  
-    menuBtn.forEach(item => item.addEventListener('click', showSidebar));
-    bodyOverlay.addEventListener('click', showSidebar);
-  });
+// to display sidebar for smaller media screens
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.querySelectorAll(".menu-btn");
+  const sidebar = document.querySelector(".sidebar");
+  const bodyOverlay = document.querySelector(".body-overlay");
+  const contentArea = document.getElementById("content-area");
+
+  const showSidebar = () => {
+    sidebar.classList.toggle('active');
+    contentArea.classList.toggle('active');
+    sidebar.classList.toggle('show-nav');
+    bodyOverlay.classList.toggle('show-nav');
+  };
+
+  menuBtn.forEach(item => item.addEventListener('click', showSidebar));
+  bodyOverlay.addEventListener('click', showSidebar);
+});
